b841a6e73eb932b0107e5560c9e4e400e3868900
AzeemB5 <xx.gold.xx262@gmail.com>
2025-07-17 19:24:13 -0400

    Update Messagebot.py

--- Messagebot.py
+++ Messagebot.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 from keep_alive import keep_alive
 
 intents = discord.Intents.default()
-intents.messages = True
+intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
